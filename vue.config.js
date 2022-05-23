ceb7a816f0de5d81c5a331bbb81224c4dec72678
alan-wu <alan.wu@auckland.ac.nz>
2022-05-23 14:25:23 +1200

    Fix a bug causing error on sparc-app build process.

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,17 @@ module.exports = {
       .use('vue-svg-inline-loader')
         .loader('vue-svg-inline-loader')
         .end()
-  },
+    const fontsRule = config.module.rule('fonts')
+    fontsRule.uses.clear()
+    config.module
+      .rule('fonts')
+      .test(/\.(ttf|otf|eot|woff|woff2)$/)
+      .use('base64-inline-loader')
+      .loader('base64-inline-loader')
+      .tap(options => {
+        // modify the options...    
+        return options
+      })
+      .end()
+  }
 }
