ba66e2d97e6a03487fa87a9644c36c65879ad019
ddjnw1yu <david.jianwei.yu@outlook.com>
2024-01-16 16:35:09 +1300

    vue-svg-inline-plugin config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueSvgInlinePlugin from 'vue-svg-inline-plugin';
+import "vue-svg-inline-plugin/src/polyfills";
+
+const app = createApp(App);
+
+app.use(VueSvgInlinePlugin, {
+    attributes: {
+        data: ["src"],
+        remove: ["alt"]
+    }
+});
+
+app.mount("#app");
